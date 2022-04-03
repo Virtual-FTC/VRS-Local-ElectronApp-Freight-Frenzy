91a622a3dd15f7a3c3b8c747ce7358366367e211
Mihir Chauhan <39073758+mihir-chauhan@users.noreply.github.com>
2022-04-03 03:43:52 -0700

    Color sensor get color properties are now mapped to respective functions and the Unity game updates for color sensor should be updating the values when the JSON data packet is fetched

--- robotConfigs/configSetup.js
+++ robotConfigs/configSetup.js
@@ -4,23 +4,23 @@ var robotConfig = null;
 var client = new XMLHttpRequest();
 client.open('GET', './robotConfigs/defaultRobot.json');
 client.onload = function () {
-    var robotConfigTxt = client.responseText;
-    if (robotConfigTxt !== '' && robotConfig == null) {
+	var robotConfigTxt = client.responseText;
+	if (robotConfigTxt !== '' && robotConfig == null) {
 		robotConfig = JSON.parse(robotConfigTxt);
 		setTimeout(variableUpdate, 1);
 		setTimeout(setupCategories, 250);
-    }
+	}
 }
 client.send();
 
 //Dropdowns for Blocks Programs
 function createDcMotorDropdown() {
-    var CHOICES = [];
+	var CHOICES = [];
 	for (i = 0; i < robotConfig["motors"].length; i++)
 		CHOICES[i] = [robotConfig["motors"][i]["name"], "dcMotor" + i];
 	if (CHOICES.length == 0)
 		CHOICES[0] = ["<None>", "dcMotor0"];
-    return new Blockly.FieldDropdown(CHOICES);
+	return new Blockly.FieldDropdown(CHOICES);
 }
 
 function createDcMotorExDropdown() {
@@ -30,86 +30,86 @@ function createDcMotorExDropdown() {
 			CHOICES[i] = [robotConfig["motors"][i]["name"], "dcMotor" + i];
 	if (CHOICES.length == 0)
 		CHOICES[0] = ["<None>", "dcMotor0"];
-    return new Blockly.FieldDropdown(CHOICES);
+	return new Blockly.FieldDropdown(CHOICES);
 }
 
 function createCRServoDropdown() {
-    var CHOICES = [];
+	var CHOICES = [];
 	for (i = 0; i < robotConfig["servos"].length; i++)
 		if (robotConfig["servos"][i]["type"] == "continous")
 			CHOICES[CHOICES.length] = [robotConfig["servos"][i]["name"], "servo" + i];
 	if (CHOICES.length == 0)
 		CHOICES[0] = ["<None>", "servo" + robotConfig["servos"].length];
-    return new Blockly.FieldDropdown(CHOICES);
+	return new Blockly.FieldDropdown(CHOICES);
 }
 
 function createServoDropdown() {
-    var CHOICES = [];
+	var CHOICES = [];
 	for (i = 0; i < robotConfig["servos"].length; i++)
 		if (robotConfig["servos"][i]["type"] == "180degrees")
 			CHOICES[CHOICES.length] = [robotConfig["servos"][i]["name"], "servo" + i];
 	if (CHOICES.length == 0)
 		CHOICES[0] = ["<None>", "servo" + robotConfig["servos"].length];
-    return new Blockly.FieldDropdown(CHOICES);
+	return new Blockly.FieldDropdown(CHOICES);
 }
 
 function createDistanceSensorDropdown() {
-    var CHOICES = [];
+	var CHOICES = [];
 	for (i = 0; i < robotConfig["distSensor"].length; i++)
 		CHOICES[i] = [robotConfig["distSensor"][i]["name"], "distanceSensor" + i];
 	if (CHOICES.length == 0)
 		CHOICES[0] = ["<None>", "distanceSensor0"];
-    return new Blockly.FieldDropdown(CHOICES);
+	return new Blockly.FieldDropdown(CHOICES);
 }
 
 function createBNO055IMUDropdown() {
-    var CHOICES = [];
+	var CHOICES = [];
 	for (i = 0; i < robotConfig["IMU"].length; i++)
 		CHOICES[i] = [robotConfig["IMU"][i]["name"], "imu" + i];
 	if (CHOICES.length == 0)
 		CHOICES[0] = ["<None>", "imu0"];
-    return new Blockly.FieldDropdown(CHOICES);
+	return new Blockly.FieldDropdown(CHOICES);
 }
 
 function createColorRangeSensorDropdown() {
-    var CHOICES = [];
+	var CHOICES = [];
 	for (i = 0; i < robotConfig["colorSensor"].length; i++)
 		CHOICES[i] = [robotConfig["colorSensor"][i]["name"], "colorSensor" + i];
 	if (CHOICES.length == 0)
 		CHOICES[0] = ["<None>", "colorSensor0"];
-    return new Blockly.FieldDropdown(CHOICES);
+	return new Blockly.FieldDropdown(CHOICES);
 }
 
 function createTouchSensorDropdown() {
-    var CHOICES = [];
+	var CHOICES = [];
 	for (i = 0; i < robotConfig["touchSensor"].length; i++)
 		CHOICES[i] = [robotConfig["touchSensor"][i]["name"], "touchSensor" + i];
 	if (CHOICES.length == 0)
 		CHOICES[0] = ["<None>", "touchSensor0"];
-    return new Blockly.FieldDropdown(CHOICES);
+	return new Blockly.FieldDropdown(CHOICES);
 }
 
 //Other Dropdowns
 function createLanguageCodeDropdown() {
-    var CHOICES = [
-        ['en', 'en'],
-    ];
-    return createFieldDropdown(CHOICES);
+	var CHOICES = [
+		['en', 'en'],
+	];
+	return createFieldDropdown(CHOICES);
 }
 
 var LANGUAGE_CODE_TOOLTIPS = [
-    ['en', 'The language code for English.'],
+	['en', 'The language code for English.'],
 ];
 
 function createCountryCodeDropdown() {
-    var CHOICES = [
-        ['US', 'US'],
-    ];
-    return createFieldDropdown(CHOICES);
+	var CHOICES = [
+		['US', 'US'],
+	];
+	return createFieldDropdown(CHOICES);
 }
 
 var COUNTRY_CODE_TOOLTIPS = [
-    ['US', 'The country code for United States.'],
+	['US', 'The country code for United States.'],
 ];
 
 //String replacement for named devices
@@ -150,59 +150,59 @@ function blocklyNaming(str) {
 function setupCategories() {
 	document.getElementById("blockSelect").value = 'BasicAutoOpMode';
 	sampleProgram(true);
-	
+
 	try {
 		var toolbox = Blockly.getMainWorkspace().getToolbox();
 	} catch (e) {
 		location.reload();
 	}
-	
+
 	var crServos = 0;
 	for (i = 0; i < robotConfig["servos"].length; i++)
 		if (robotConfig["servos"][i]["type"] == "continous")
 			crServos++;
-	
+
 	if (crServos == 0)
 		toolbox.getToolboxItemById('CRServo').hide();
 	if (robotConfig["servos"].length - crServos == 0)
 		toolbox.getToolboxItemById('Servo').hide();
-	
+
 	if (robotConfig["motors"].length == 0)
-        toolbox.getToolboxItemById('Motor').hide();
+		toolbox.getToolboxItemById('Motor').hide();
 	if (robotConfig["motors"].length < 2)
-        toolbox.getToolboxItemById('MotorDual').hide();
+		toolbox.getToolboxItemById('MotorDual').hide();
 	if (robotConfig["motors"].length < 4)
-        toolbox.getToolboxItemById('MotorQuad').hide();
-	
+		toolbox.getToolboxItemById('MotorQuad').hide();
+
 	var motorsEx = 0;
 	for (i = 0; i < robotConfig["motors"].length; i++)
 		if (robotConfig["motors"][i]["type"] == "extended")
 			motorsEx++;
-	
+
 	if (motorsEx == 0)
-        toolbox.getToolboxItemById('MotorEx').hide();
+		toolbox.getToolboxItemById('MotorEx').hide();
 	if (motorsEx < 2)
-        toolbox.getToolboxItemById('MotorExDual').hide();
+		toolbox.getToolboxItemById('MotorExDual').hide();
 	if (motorsEx < 4)
-        toolbox.getToolboxItemById('MotorExQuad').hide();
-	
+		toolbox.getToolboxItemById('MotorExQuad').hide();
+
 	if (robotConfig["motors"].length == 0 && robotConfig["servos"].length == 0)
-        toolbox.getToolboxItemById('Actuators').hide();
-	
+		toolbox.getToolboxItemById('Actuators').hide();
+
 	if (robotConfig["distSensor"].length == 0)
-        toolbox.getToolboxItemById('DistanceSensor').hide();
-	
+		toolbox.getToolboxItemById('DistanceSensor').hide();
+
 	if (robotConfig["IMU"].length == 0) {
 		toolbox.getToolboxItemById('IMUSensor').hide();
 		toolbox.getToolboxItemById('IMUParamSensor').hide();
 	}
-	
+
 	if (robotConfig["colorSensor"].length == 0)
 		toolbox.getToolboxItemById('ColorSensor').hide();
-	
+
 	if (robotConfig["touchSensor"].length == 0)
 		toolbox.getToolboxItemById('TouchSensor').hide();
-	
+
 	if (robotConfig["distSensor"].length == 0 && robotConfig["IMU"].length == 0 && robotConfig["colorSensor"].length == 0 && robotConfig["touchSensor"].length == 0)
-        toolbox.getToolboxItemById('Sensors').hide();
+		toolbox.getToolboxItemById('Sensors').hide();
 }
